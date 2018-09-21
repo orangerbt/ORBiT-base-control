faa7b65a487ca2b65d9c67edf2fce335245a4fa2
mike <qazwqsx9@gmail.com>
2018-09-21 12:06:43 -0400

    Fix update problem

--- src/org/orbit/rocket/gui/DataListener.java
+++ src/org/orbit/rocket/gui/DataListener.java
@@ -2,6 +2,6 @@ package org.orbit.rocket.gui;
 
 public abstract class DataListener {
 	 abstract boolean verify(String sensorKey);
-	 abstract void action(String sensorKey,String data);
-	 abstract void action(String sensorKey,String sensorName,String sensorUnit,String data);//Action method will be executed by order
+	 abstract void action(String sensorKey,String data,long time);
+	 abstract void action(String sensorKey,String sensorName,String sensorUnit,String data,long time);//Action method will be executed by order
 }
